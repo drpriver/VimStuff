a8d68aeb22d68986b97a34354e77cf5baee4c876
drpriver <priver.david@gmail.com>
2021-03-01 17:48:14 -0500

    Update install script

--- install.py
+++ install.py
@@ -1,11 +1,21 @@
-# assumes this in ~/.vim
+# assumes this in ~/.vim/VimFiles
 
 import shutil
 import glob
-shutil.copy('vimrc', '../.vimrc')
+import os
+def do_it():
+    shutil.copy('vimrc', '../.vimrc')
+    pyfiles = glob.glob('*.py')
+    pyfiles.remove('install.py')
+    for f in pyfiles:
+        shutil.copy(f, '..')
+    os.makedirs('../autoload', exist_ok=True)
+    os.makedirs('../plugin', exist_ok=True)
+    os.makedirs('../plugged', exist_ok=True)
+    os.makedirs('../undo', exist_ok=True)
+    os.makedirs('../swap', exist_ok=True)
+    shutil.copy('plug.vim', '../autoload/')
+    shutil.copy('beffer.vim', '../plugin/')
 
-pyfiles = glob.glob('*.py')
-pyfiles.remove('install.py')
-for f in pyfiles:
-    shutil.copy(f, '..')
-
+if __name__ == '__main__':
+    do_it()
